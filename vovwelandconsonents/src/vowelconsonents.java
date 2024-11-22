073171864ac82a10fc00eaedc9d628be6ad2fdaa
Pushpa Yadav <155540856+PushpaYadav09@users.noreply.github.com>
2024-11-22 21:17:08 +0530

    Add files via upload

---
+++ vovwelandconsonents/src/vowelconsonents.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.*;
+public class vowelconsonents{
+    public static void Boolean(char c){
+        c= character.tolowecase(c);
+    }
+
+    public static void main(String args[]) {
+        int vowelcount=0;
+        int consonentcount=0;
+        Scanner sn = new Scanner(System.in);
+
+        String name = sn.nextLine();
+
+        StringBuilder sb=new StringBuilder(name);
+
+
+
+            System.out.println(name+"\n"+"lenght of string= "+name.length());
+            
+
+        }
+    }
